db088b58ebc4452a4cac45abb640e5255235d2f3
Arundeep Singh Bains <77286522+ArundeepSinghBains@users.noreply.github.com>
2021-01-25 20:41:27 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,57 @@
+let a = 3;
+let b = 5;
+let c;
+
+let output = '';
+output += 'let a = 3;';
+output += '\n';
+output += 'let b = 5;';
+output += '\n';
+output += 'let c;';
+output += '\n';
+output += '-----------------';
+output +='\n';
+output += 'a + b = ' + (a + b);
+output +='\n';
+output += 'a - b = ' + (a - b)
+output +='\n';
+output += 'a * b = ' + (a * b)
+output +='\n';
+output += 'a / b = ' + (a / b)
+output +='\n';
+output += 'a % b = ' + (a % b)
+output +='\n';
+output += 'a += b = ' + (a += b)
+output +='\n';
+output += 'a -= b = ' + (a -= b)
+output +='\n';
+output += 'a *= b = ' + (a *= b)
+output +='\n';
+output += 'a /= b = ' + (a /= b)
+output +='\n';
+output += 'a %= b = ' + (a %= b)
+output +='\n';
+output += 'a == b = ' + (a == b)
+output +='\n';
+output += 'a != b = ' + (a != b)
+output +='\n';
+output += 'a > b = ' + (a > b)
+output +='\n';
+output += 'a < b = ' + (a < b)
+output +='\n';
+output += '!a && !c = ' + (!a && !c )
+output +='\n';
+output += '!a || !c = ' + (!a || !c )
+output +='\n';
+
+alert(output);
+
+let first_name = "Arundeep";
+let last_name = "Bains";
+let email = "bain0080@algonquinlive.com";
+
+let task2 = '';
+task2 +='\n';
+task2 += 'My name is ' + (first_name) + " " + (last_name) + ".";
+task2 += ' You can contact me at ' + (email) + ".";
+alert(task2);
